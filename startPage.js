ca7a3ac8995b224bca057da309c876622443cb60
Pythagean <jeremysto@gmail.com>
2015-07-28 10:17:13 +1200

    Fixed asynchronous get problem, Active Period
    Works out active period by comparing current time to period start/end
    times

--- startPage.js
+++ startPage.js
@@ -3,6 +3,7 @@ function openStartUpTabs() {
   
   var numberPeriods = 0;
   var enabledData = "";
+  var descData = "";
   var startData = "";
   var endData = "";
   var urlsData = "";
@@ -12,18 +13,21 @@ function openStartUpTabs() {
   var currentHour = new Date().getHours();
   var currentMinute = new Date().getMinutes();
   
-  window.alert("currentTime: " + currentHour + ":" + currentMinute);
+ // window.alert("currentTime: " + currentHour + ":" + currentMinute);
   
   chrome.storage.sync.get("numberPeriods", function(data){
     numberPeriods = data['numberPeriods'] + 1;
     for (var i = 1; i < numberPeriods+1; i++) {
       (function(x) {
         var enabled = "'period" + x + "_enabled'";
+        var desc = "'period" + x + "_desc'";
         var start = "'period" + x + "_start'";
         var end = "'period" + x + "_end'";
         var urls = "'period" + x + "_urls'";
         
-        //Retrieve data from storage
+        
+        //PROBLEM here with asynchronous gets
+        
         chrome.storage.sync.get(enabled, function(data){
           enabledData = data[enabled];
           if (enabledData == "false") {
@@ -36,18 +40,44 @@ function openStartUpTabs() {
           
         });
         
+        chrome.storage.sync.get(desc, function(data){
+          descData = data[desc];
+          
+        });
+        
         chrome.storage.sync.get(end, function(data){
           endData = data[end];
         });
         
         chrome.storage.sync.get(urls, function(data){
           urlsData = data[urls];
+          
+          if (startData != "-" && endData != "-") {
+            var periodHourStart = startData.split(':')[0];
+            var periodMinuteStart = startData.split(':')[1];
+            var periodHourEnd = endData.split(':')[0];
+            var periodMinuteEnd = endData.split(':')[1];
+            
+            if (currentHour > periodHourStart && currentHour < periodHourEnd) {
+              window.alert(descData + " period active");
+            }
+            if (currentHour == periodHourStart && currentMinute > periodMinuteStart) {
+              window.alert(descData + " period active");
+            }
+            if (currentHour == periodHourEnd && currentMinute < periodMinuteEnd) {
+              window.alert(descData + " period active");
+            }
+            
+            //window.alert("period" + x + "= " + periodHourStart + ":" + periodMinuteStart + " to " + periodHourEnd + ":" + periodMinuteEnd);
+          }
+          
         });
         
-        var periodHourStart = startData.split(':')[0];
-        var periodMinuteStart = startData.split(':')[1];
-        var periodHourStart = endData.split(':')[0];
-        var periodMinuteEnd = endData.split(':')[1];
+        
+        
+        
+        
+        
         
         
       })(i);
